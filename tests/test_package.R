7796a3faf97b4fe1c2e5ceb5648272a2c3c2b7c6
Romain GUEDON <romain.guedon@eleves.ec-nantes.fr>
2018-07-16 16:19:10 +0100

    some changes

--- tests/test_package.R
+++ tests/test_package.R
@@ -1,43 +1,21 @@
-load("./data/data.RData")
+#test_package.R
+
+library(subtypr)
 
 grid.support <- list(K = 5, alpha = 0.5, cluster.number = 2)
 
 data.list <- lapply(datasets, t)
 
-
-
-
-
-
-
-
-# formals(GetMethodInfo("anf")$Func)
-grid.support <- list(cluster.number = 2:3,
+grid.support <- list(cluster.number = 4,
                      k.affi = 5,
                      K.fusion = 5)
 
-tuning.result <- Tuning(data.list = data.list, method = "anf",
+tuning.result <- Tuning(data.list = datasets, method = "anf",
                         grid.support = grid.support, metric = "nmi",
-                        ground.truth = real.groundtruth, parallel = FALSE, plot = TRUE)
+                        ground.truth = real.groundtruth, parallel = TRUE, plot = TRUE)
 
 
-
-# method <- GetMethodInfo("anf")
-# all.args <- as.list(base::formals(method$Func))
-# all.args[names(grid.support)] <- grid.support
-#
-# # remove data.list from the args
-# i.data <- which(names(all.args) == "data.list")
-# all.args <- all.args[-i.data]
-# print("all.atgs = ")
-# print(all.args)
-#
-# # span the grid
-# grid <- base::expand.grid(all.args, stringsAsFactors = )
-# l <- dim(grid)[1]
-# print("grid  = ")
-# print(grid)
-#
+MetricValues()
 
 
 
