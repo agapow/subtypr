99b2801f0df25444bbdb99281d515c57c76e75d4
Romain GUEDON <romain.guedon@eleves.ec-nantes.fr>
2018-08-07 12:39:44 +0100

    some tests added

---
+++ tests/test_generate.R
@@ -0,0 +1,39 @@
+library(SNFtool)
+library(subtypr)
+
+snf <- get_method("snf")
+
+# Check partitions:
+partitions <- generate_partitions(610, 3)
+for (i in 1:3){
+  plot(partitions$partitions[i, ])
+}
+
+sv
+for (i in 1:3){
+  print(dim(svd(breast_cancer_data$data_list[[2]])[[i]]))
+}
+
+# Check generation:
+multi <- generate_multi_structured(data_support = breast_cancer_data$data_list,
+                                    structure_type = "moclus",
+                                    sd_signal = 10,
+                                    sparse = TRUE,
+                                    percent_sparsity = c(0.5, 0, 0.5))
+
+
+
+
+
+# Check distribution of simulated data, compared to original one:
+check_distribution(multi[[1]][[1]])
+check_distribution(breast_cancer_data$data_list[[1]])
+
+
+
+snfx <- snf(data_list = multi$data_generated, cluster_number = 4, K = 10, alpha = 0.3, t = 10)
+
+
+SNFtool::displayClusters(snfx$affinity_fused, snfx$partition)
+
+normalized_mutual_information(snfx$partition, multi$partition_tot)
---
+++ tests/test_silhouette.R
@@ -0,0 +1,26 @@
+
+
+tunex <- tuning(data_list = breast_cancer_data$data_list,
+                method = "snf",
+                grid_support = list(cluster_number = 4),
+                metric = "asw_affinity",
+                true_partition = NULL,
+                parallel = F)
+
+
+affi <- SNFtool::affinityMatrix(SNFtool::dist2(breast_cancer_data$data_list[[1]],
+                                               breast_cancer_data$data_list[[1]]))
+partition <- breast_cancer_data$partition
+
+
+asw <- get_metric("asw_affinity")
+get <- asw$metric(pred_partition = breast_cancer_data$partition, data_for_metric = affi)
+
+
+sil <- silhouette_affinity(pred_partition = breast_cancer_data$partition, affinity_matrix = affi)
+class(sil)
+
+
+summary(object = silhouette_affinity(pred_partition = breast_cancer_data$partition, affinity_matrix = affi))$avg.width
+
+
