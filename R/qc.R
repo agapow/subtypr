fb097822f7e4d2c7812c31355577997a1c31adec
Paul Agapow <paul@agapow.net>
2018-08-09 16:03:15 +0100

    fixed qc functions

--- R/qc.R
+++ R/qc.R
@@ -2,9 +2,10 @@
 #'
 #' Many methods have restrictions on the data they can use (most notably
 #' missing data). This is a simple function for assessing the quality of
-#' input data, which allows the trigger condition to be modified.
+#' input data. By default it looks for missing (NA) data although the
+#' the trigger condition can be modified.
 #'
-#' @param m a feature matrix with samples in rows and features in columns
+#' @param m a sample-feature matrix
 #' @param flag_fxn a boolean function that detects problematic data
 #'
 #' @return a summary of how the matrix is affected by problematic data
@@ -18,16 +19,18 @@ flag_data <- function(m, flag_fxn = is.na) {
   ## Main:
   cols_with_flagged_data <- unname(apply(m, MARGIN = 2, function(x) sum(flag_fxn(x))))
   rows_with_flagged_data <- unname(apply(m, MARGIN = 1, function(x) sum(flag_fxn(x))))
-  flagged_data <- sum(rows_with_flagged_data)
 
-  data_less_flagged <- m[!rows_with_flagged_data, !cols_with_flagged_data]
+  flagged_data_cnt <- sum(rows_with_flagged_data)
+  at_assert(flagged_data_cnt == sum(cols_with_flagged_data))
+
+  data_less_flagged <- subset (m, !rows_with_flagged_data, !cols_with_flagged_data)
 
   ## Postconditions & return:
   return(
-    c(
-      flagged_data_cnt = flagged_data,
-      flagged_rows_cnt = unname(rows_with_flagged_data),
-      flagged_cols_cnt = unname(cols_with_flagged_data),
+    list (
+      flagged_data_cnt = flagged_data_cnt,
+      flagged_rows_cnt = sum (as.logical (rows_with_flagged_data)),
+      flagged_cols_cnt = sum (as.logical (cols_with_flagged_data)),
       dims_without_flagged_data = dim (data_less_flagged)
     )
   )
@@ -50,7 +53,7 @@ filter_flagged_data <- function(m, flag_fxn = is.na) {
   # TODO: does this work if it reduces the matrix to nothing
 
   ## Preconditions:
-  at_assert(is.function(flag_fxn))
+  at_assert (is.function(flag_fxn))
 
   ## Main:
   cols_with_flagged_data <- unname(apply(m, MARGIN = 2, function(x) sum(flag_fxn(x))))
@@ -58,9 +61,12 @@ filter_flagged_data <- function(m, flag_fxn = is.na) {
 
   ## Postconditions & return:
   return(
-    m[!rows_with_flagged_data, !cols_with_flagged_data]
+    subset (m, !rows_with_flagged_data, !cols_with_flagged_data)
   )
 }
 
 
 # TODO: also check in multi-omics that each matrix has same samples
+# TODO: transform flagged data
+# TODO: split negative matrix
+
