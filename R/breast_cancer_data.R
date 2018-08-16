aa2af58cd3c83539694ae244c0cff877f7551bde
Romain GUEDON <romain.guedon@eleves.ec-nantes.fr>
2018-08-16 14:48:18 +0100

    added test set

--- R/breast_cancer_data.R
+++ R/breast_cancer_data.R
@@ -4,7 +4,8 @@
 #'   \url{https://cancergenome.nih.gov/}.
 #'
 #'
-#' @format The data contains two lists:
+#' @format The data contains a training and a testing set, resulting in
+#'   4 elements:
 #'
 #' * $data_list that contains:
 #'
@@ -13,6 +14,15 @@
 #'   * $miRNA: micro RNA expression
 #'
 #' * $partition: the classification of patients according to
+#'   \url{https://doi.org/10.1073/pnas.191367098}
+#'
+#' * $test_data_list that contains:
+#'
+#'   * $mRNA: mRNA expression
+#'   * $methylation: methylation profile
+#'   * $miRNA: micro RNA expression
+#'
+#' * $test_partition: the classification of patients according to
 #'   \url{https://doi.org/10.1073/pnas.191367098}
 #'
 #'
